15b3f52340511a24870746b9695539f64ae91dd7
Masayuki Sato <masa@tohoku.ac.jp>
2022-03-31 11:07:54 +0900

    remove echo

--- sync_inner.bash
+++ sync_inner.bash
@@ -4,5 +4,5 @@ set -euo pipefail
 
 mkdir -p ${RSYNC_LOG_DIR}
 mkdir -p ${BACKUP_DIR}
-echo rsync ${RSYNC_OPT} --exclude-from=${RSYNC_EXCLUDE_FILE} ${ORIG_DIR} ${BACKUP_DIR} > ${RSYNC_LOG_FILE} 2>&1
+rsync ${RSYNC_OPT} --exclude-from=${RSYNC_EXCLUDE_FILE} ${ORIG_DIR} ${BACKUP_DIR} > ${RSYNC_LOG_FILE} 2>&1
 xz ${RSYNC_LOG_FILE} 
