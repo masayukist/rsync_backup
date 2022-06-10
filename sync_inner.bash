49b43468b8e80d499206b784baa998732d0e4d69
root <root@webserv.cal.is.tohoku.ac.jp>
2022-06-10 10:32:39 +0900

    log improved

--- sync_inner.bash
+++ sync_inner.bash
@@ -2,7 +2,17 @@
 
 set -euo pipefail
 
+sync_inner_func () {
+	DATE_START=`date`
+	rsync ${RSYNC_OPT} --exclude-from=${RSYNC_EXCLUDE_FILE} ${ORIG_DIR} ${BACKUP_DIR}
+	echo
+	echo started : ${DATE_START}
+	echo finished: `date`
+}
+
 mkdir -p ${RSYNC_LOG_DIR}
 mkdir -p ${BACKUP_DIR}
-rsync ${RSYNC_OPT} --exclude-from=${RSYNC_EXCLUDE_FILE} ${ORIG_DIR} ${BACKUP_DIR} > ${RSYNC_LOG_FILE} 2>&1
-xz ${RSYNC_LOG_FILE} 
+
+sync_inner_func > ${RSYNC_LOG_FILE} 2>&1
+
+xz ${RSYNC_LOG_FILE}
